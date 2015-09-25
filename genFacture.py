ed8cf5427a9240f59fa0b2d5f475759b1604671f
arctarus <arthur.dewarumez@etudiant.univ-lille1.fr>
2015-09-25 17:41:40 +0200

    Change file extension of the generated file to ini. Add the transaction id in the invoice

--- genFacture.py
+++ genFacture.py
@@ -1,11 +1,18 @@
 import configparser
 import sys
+import os
+import random
+
+SEED_LENGTH = 10
+TRANSACTION_ID_LENGTH = 1024
 
 def genFacture(nom,montant,ordre):
 
 	config = configparser.RawConfigParser()
+	random.seed(os.urandom(SEED_LENGTH))
+	random.getrandbits(TRANSACTION_ID_LENGTH)
 	config['CLIENT'] = {'Nom' : nom, 'Montant' : montant, 'Ordre' : ordre}
-	with open('facture.txt','w') as facture:
+	with open('facture.ini','w') as facture:
 		config.write(facture)
 
 if __name__ == "__main__":
