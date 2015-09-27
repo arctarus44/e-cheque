f17826ea64a87f64638bc8de017e527bb6a27865
arctarus <arthur.dewarumez@gmail.com>
2015-09-27 15:38:38 +0200

    Starting the implementation of the script for the generation of a cheque. It need a lot of improvement,for example, we need to add some structures in order to check efficiently if every needed options an sections are present.
    I need to add the signature of the cheque.

---
+++ genCheque.py
@@ -0,0 +1,41 @@
+import sys
+from rsa import RSA
+from configparser import ConfigParser
+
+def read_bill():
+	"""Read a check from stdin and return an instance of ConfigParser."""
+	f = sys.stdin.readlines()
+	bill = ConfigParser()
+	bill.read_file(f)
+	return bill
+
+if __name__ == "__main__":
+	bill = read_bill()
+	user_name = sys.argv[1]
+
+	if not bill.has_section("CLIENT"):
+		print("Missing section client", file=sys.stderr)
+		exit(1)
+	if not bill.has_option("CLIENT", "nom"):
+		print("Missing option name in the client section", file=sys.stderr)
+		exit(1)
+	if not bill.has_option("CLIENT", "montant"):
+		print("Missing option amount in the client section", file=sys.stderr)
+		exit(1)
+	if not bill.has_option("CLIENT", "ordre"):
+		print("Missing option ordre in the client section", file=sys.stderr)
+		exit(1)
+	if not bill.has_option("CLIENT", "transaction"):
+		print("Missing option transaction in the client section", file=sys.stderr)
+		exit(1)
+
+	if bill.get("CLIENT", "nom") != user_name:
+		print("This cheque is not for me !", sys.stderr)
+		exit(1)
+
+	cheque = ConfigParser()
+	cheque.add_section("cheque")
+	cheque.set("cheque", "depositor", user_name)
+	cheque.set("cheque", "beneficiary", bill.get("CLIENT", "ordre"))
+	cheque.set("cheque", "amount", bill.get("CLIENT", "montant"))
+	cheque.set("cheque", "transaction_id", bill.get("CLIENT", "transaction"))
