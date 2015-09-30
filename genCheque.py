af661628a2eaa6169f669e00549019845157fdbd
denism2 <denism2@pcmt19.m2fil.univ-lille1.fr>
2015-09-30 07:49:30 +0200

    synchronization with bill

--- genCheque.py
+++ genCheque.py
@@ -16,20 +16,20 @@ if __name__ == "__main__":
 	if not bill.has_section("CLIENT"):
 		print("Missing section client", file=sys.stderr)
 		exit(1)
-	if not bill.has_option("CLIENT", "nom"):
+	if not bill.has_option("CLIENT", "Name"):
 		print("Missing option name in the client section", file=sys.stderr)
 		exit(1)
-	if not bill.has_option("CLIENT", "montant"):
+	if not bill.has_option("CLIENT", "Amount"):
 		print("Missing option amount in the client section", file=sys.stderr)
 		exit(1)
-	if not bill.has_option("CLIENT", "ordre"):
+	if not bill.has_option("CLIENT", "To"):
 		print("Missing option ordre in the client section", file=sys.stderr)
 		exit(1)
-	if not bill.has_option("CLIENT", "transaction"):
+	if not bill.has_option("CLIENT", "Transaction"):
 		print("Missing option transaction in the client section", file=sys.stderr)
 		exit(1)
 
-	if bill.get("CLIENT", "nom") != user_name:
+	if bill.get("CLIENT", "Name") != user_name:
 		print("This cheque is not for me !", sys.stderr)
 		exit(1)
 
@@ -38,4 +38,5 @@ if __name__ == "__main__":
 	cheque.set("cheque", "depositor", user_name)
 	cheque.set("cheque", "beneficiary", bill.get("CLIENT", "To"))
 	cheque.set("cheque", "amount", bill.get("CLIENT", "Amount"))
-	cheque.set("cheque", "transaction_id", bill.get("CLIENT", "transaction"))
+	cheque.set("cheque", "transaction_id", bill.get("CLIENT", "Transaction"))
+	#ajout 
