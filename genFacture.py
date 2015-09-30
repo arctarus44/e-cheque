8fc451a57ad8ce257e50254ac1f66ffa74bfd1aa
denism2 <denism2@pcmt19.m2fil.univ-lille1.fr>
2015-09-30 13:13:12 +0200

    bill name with id transaction

--- genFacture.py
+++ genFacture.py
@@ -10,11 +10,12 @@ def genFacture(name,amount,to):
 
 	config = configparser.RawConfigParser()
 	random.seed(os.urandom(SEED_LENGTH))
-	config['CLIENT'] = {'Name' : name, 'Amount' : amount, 'To' : to, 'Transaction' : hex(random.getrandbits(TRANSACTION_ID_LENGTH))}
-	with open('seller/facture.ini','w') as facture:
+	id_transac = hex(random.getrandbits(TRANSACTION_ID_LENGTH))
+	config['CLIENT'] = {'Name' : name, 'Amount' : amount, 'To' : to, 'Transaction' : id_transac}
+	with open('seller/facture_'+id_transac+'.ini','w') as facture:
 		config.write(facture)
 		facture.close()
-	os.rename('seller/facture.ini','customers/'+name+'/facture.ini')
+	os.rename('seller/facture_'+id_transac+'.ini','customers/'+name+'/facture_'+id_transac+'.ini')
 	
 if __name__ == "__main__":
 	genFacture(sys.argv[1],sys.argv[2],sys.argv[3])
