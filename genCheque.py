28e69ee3a1be98aeef126d1895317d86ed17bbc7
denism2 <denism2@pcmt19.m2fil.univ-lille1.fr>
2015-09-30 11:40:41 +0200

    new version

--- genCheque.py
+++ genCheque.py
@@ -2,40 +2,46 @@ import sys
 from rsa import RSA
 from configparser import ConfigParser
 
+user_name = sys.argv[1]
 def read_bill():
 	"""Read a check from stdin and return an instance of ConfigParser."""
-	f = sys.stdin.readlines()
+#	f = sys.stdin.readlines()
+#os.path.join
 	bill = ConfigParser()
-	bill.read_file(f)
+	bill.read_file('/customers/'+user_name+'/facture.ini')
 	return bill
 
 if __name__ == "__main__":
 	bill = read_bill()
-	user_name = sys.argv[1]
 
 	if not bill.has_section("CLIENT"):
 		print("Missing section client", file=sys.stderr)
 		exit(1)
-	if not bill.has_option("CLIENT", "nom"):
+	if not bill.has_option("CLIENT", "Name"):
 		print("Missing option name in the client section", file=sys.stderr)
 		exit(1)
-	if not bill.has_option("CLIENT", "montant"):
+	if not bill.has_option("CLIENT", "Amount"):
 		print("Missing option amount in the client section", file=sys.stderr)
 		exit(1)
-	if not bill.has_option("CLIENT", "ordre"):
+	if not bill.has_option("CLIENT", "To"):
 		print("Missing option ordre in the client section", file=sys.stderr)
 		exit(1)
-	if not bill.has_option("CLIENT", "transaction"):
+	if not bill.has_option("CLIENT", "Transaction"):
 		print("Missing option transaction in the client section", file=sys.stderr)
 		exit(1)
 
-	if bill.get("CLIENT", "nom") != user_name:
+	if bill.get("CLIENT", "Name") != user_name:
 		print("This cheque is not for me !", sys.stderr)
 		exit(1)
 
 	cheque = ConfigParser()
 	cheque.add_section("cheque")
 	cheque.set("cheque", "depositor", user_name)
-	cheque.set("cheque", "beneficiary", bill.get("CLIENT", "ordre"))
-	cheque.set("cheque", "amount", bill.get("CLIENT", "montant"))
-	cheque.set("cheque", "transaction_id", bill.get("CLIENT", "transaction"))
+	cheque.set("cheque", "beneficiary", bill.get("CLIENT", "To"))
+	cheque.set("cheque", "amount", bill.get("CLIENT", "Amount"))
+	cheque.set("cheque", "transaction_id", bill.get("CLIENT", "Transaction"))
+	with open('customers/'+user_name+'/cheque.ini','w') as cheque:
+		config.write(cheque)
+		cheque.close()
+	os.rename('customers/'+user_name+'/cheque.ini', 'seller/cheque.ini')	
+	exit(0)
