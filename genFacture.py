87701db89b4ab3b38f231ae52218702e9e2e1fda
denism2 <denism2@pcmt19.m2fil.univ-lille1.fr>
2015-09-30 13:31:14 +0200

    bill name with date of creation

--- genFacture.py
+++ genFacture.py
@@ -2,20 +2,28 @@ import configparser
 import sys
 import os
 import random
+from datetime import datetime, date, time
 
 SEED_LENGTH = 10
 TRANSACTION_ID_LENGTH = 1024
-
+	
 def genFacture(name,amount,to):
 
 	config = configparser.RawConfigParser()
 	random.seed(os.urandom(SEED_LENGTH))
-	id_transac = hex(random.getrandbits(TRANSACTION_ID_LENGTH))
+	
+	dt = datetime.now()
+	dn = ""
+	for i in dt:
+		dn += dt[i]
+
+	id_transac = hex(random.getrandbits(TRANSACTION_ID_LENGTH))[2:]
+
 	config['CLIENT'] = {'Name' : name, 'Amount' : amount, 'To' : to, 'Transaction' : id_transac}
-	with open('seller/facture_'+id_transac+'.ini','w') as facture:
+	with open('seller/facture_'+id_transac+'_'+dn+'.ini','w') as facture:
 		config.write(facture)
 		facture.close()
-	os.rename('seller/facture_'+id_transac+'.ini','customers/'+name+'/facture_'+id_transac+'.ini')
+	os.rename('seller/facture_'+id_transac+'_'+dn+'.ini','customers/'+name+'/facture_'+id_transac+'_'+dn+'.ini')
 	
 if __name__ == "__main__":
 	genFacture(sys.argv[1],sys.argv[2],sys.argv[3])
