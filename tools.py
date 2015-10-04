1761276fa2f3d37333c3e73704a1af32f02e62ef
arctarus <arthur.dewarumez@gmail.com>
2015-10-04 21:07:11 +0200

    klfg

--- tools.py
+++ tools.py
@@ -19,6 +19,7 @@ OPT_C_TRANS_ID = "Transaction_id"
 
 # Key file
 SCT_K_KEY = "Key"
+OPT_K_SIZE = "Size"
 OPT_K_E = "E"
 OPT_K_N = "D"
 OPT_K_D = "N"
@@ -69,20 +70,20 @@ STRCT_CHEQUE = {SCT_C_CHEQUE: [OPT_C_DRAWER,
 
 def text_to_int(text):
 	"""Convert the ascii text given as string to an integer"""
-	text = ""
-	while integer>0:
-		char = chr(integer & 0xFF)
-		text = char + text
-		integer = integer >> 8
-	return string[:-1]
-
-def int_to_text(integer):
-	"""Convert the given number as an asciistring """
 	integer = 0
 	for c in text:
 		integer = integer << 8
 		integer = integer + ord(c)
 	return integer
+
+def int_to_text(integer):
+	"""Convert the given number as an asciistring """
+	text = ""
+	while integer>0:
+		char = chr(integer & 0xFF)
+		text = char + text
+		integer = integer >> 8
+	return text[:-1]
 
 def check_config(config, structure):
 	"""Check if the ConfigParser instance respect the structure required."""
