29868c4d3239d8767aee48864a2164bd0f9eafdd
arctarus <arthur.dewarumez@gmail.com>
2015-10-04 16:44:33 +0200

    New method for the conversion of integer to text and integer to text

--- tools.py
+++ tools.py
@@ -69,15 +69,20 @@ STRCT_CHEQUE = {SCT_C_CHEQUE: [OPT_C_DRAWER,
 
 def text_to_int(text):
 	"""Convert the ascii text given as string to an integer"""
-	hex_repr = binascii.hexlify(text.encode("ascii"))
-	return int(hex_repr.decode("ascii"), 16)
+	text = ""
+	while integer>0:
+		char = chr(integer & 0xFF)
+		text = char + text
+		integer = integer >> 8
+	return string[:-1]
 
 def int_to_text(integer):
 	"""Convert the given number as an asciistring """
-	hex_repr = hex(integer)[2:]	# skipping the 0x
-	if len(hex_repr) % 2 != 0:
-		hex_repr = "0" + hex_repr
-	return binascii.unhexlify(hex_repr).decode("ascii")
+	integer = 0
+	for c in text:
+		integer = integer << 8
+		integer = integer + ord(c)
+	return integer
 
 def check_config(config, structure):
 	"""Check if the ConfigParser instance respect the structure required."""
