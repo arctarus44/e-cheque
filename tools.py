8321e3a964e4a9b7e9abfdafce469cfad29006e3
arctarus <arthur.dewarumez@gmail.com>
2015-10-05 01:20:51 +0200

    Removing useless methods

--- tools.py
+++ tools.py
@@ -68,23 +68,6 @@ STRCT_CHEQUE = {SCT_C_CHEQUE: [OPT_C_DRAWER,
 							   OPT_C_PAYEE,
 							   OPT_C_TRANS_ID]}
 
-def text_to_int(text):
-	"""Convert the ascii text given as string to an integer"""
-	integer = 0
-	for c in text:
-		integer = integer << 8
-		integer = integer + ord(c)
-	return integer
-
-def int_to_text(integer):
-	"""Convert the given number as an asciistring """
-	text = ""
-	while integer>0:
-		char = chr(integer & 0xFF)
-		text = char + text
-		integer = integer >> 8
-	return text[:-1]
-
 def check_config(config, structure):
 	"""Check if the ConfigParser instance respect the structure required."""
 
